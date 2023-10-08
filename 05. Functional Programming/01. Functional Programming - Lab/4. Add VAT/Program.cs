da8564de2df2e72e2a288149a2198e7d267f1952
Amxx79 <e.siwanov@gmail.com>
2023-10-08 14:35:15 +0300

    Functional Programming

---
+++ 05. Functional Programming/01. Functional Programming - Lab/4. Add VAT/Program.cs
@@ -0,0 +1,12 @@
+﻿Func<string, double> parser = n => double.Parse(n);
+Func<double, double> vat = x => x * 1.20;
+
+List<double> numbers = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(parser).Select(vat)
+    .ToList();
+
+foreach (double num in numbers)
+{
+    Console.WriteLine($"{num:f2}");
+}

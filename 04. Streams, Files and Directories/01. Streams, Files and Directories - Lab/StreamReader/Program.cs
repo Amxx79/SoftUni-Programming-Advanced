c72a268bb19a0bbbcd0d0441bb34ef73761371fd
Amxx79 <e.siwanov@gmail.com>
2023-10-08 14:35:00 +0300

    Streams, Files and Directories

---
+++ 04. Streams, Files and Directories/01. Streams, Files and Directories - Lab/StreamReader/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+StreamReader reader = new StreamReader(@"../../../New Text Reader.txt");
+
+string content = reader.ReadToEnd(); //Read for absolute END
+string content2 = reader.ReadLine(); //Read Only One Line
+
+
+
+using (reader)
+{
+    Console.WriteLine(content);
+}

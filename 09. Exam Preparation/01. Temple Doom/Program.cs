26923a83e4b7db737a0fd9e6231e48d52833fbcd
Amxx79 <e.siwanov@gmail.com>
2023-10-17 23:49:30 +0300

    Exam Preparation

---
+++ 09. Exam Preparation/01. Temple Doom/Program.cs
@@ -0,0 +1,68 @@
+﻿List<int> toolsInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+List<int> substancesInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+Queue<int> tools = new(toolsInput);
+Stack<int> substances = new(substancesInput);
+
+List<int> challanges = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+int resultOfMultiply;
+
+while (true)
+{
+    int toolsElement = 0;
+    int substanceElement = 0;
+    if (tools.Count == 0 || substances.Count == 0)
+    {
+        break;
+    }
+    toolsElement = tools.Dequeue();
+    substanceElement = substances.Pop();
+
+    resultOfMultiply = toolsElement * substanceElement;
+
+
+    if (challanges.Contains(resultOfMultiply))
+    {
+        challanges.Remove(resultOfMultiply);
+    }
+    else
+    {
+        toolsElement += 1;
+        tools.Enqueue(toolsElement);
+        substanceElement -= 1;
+        if (substanceElement > 0)
+        {
+            substances.Push(substanceElement);
+        }
+    }
+}
+
+if ((tools.Count == 0 || substances.Count == 0) && challanges.Count != 0)
+{
+    Console.WriteLine("Harry is lost in the temple. Oblivion awaits him.");
+
+    if (tools.Count != 0)
+    {
+        Console.WriteLine($"Tools: {string.Join(", ", tools)}");
+    }
+    if (substances.Count != 0)
+    {
+        Console.WriteLine($"Substances: {string.Join(", ", substances)}");
+    }
+    Console.WriteLine($"Challenges: {string.Join(", ", challanges)}");
+
+}
+
+if (challanges.Count == 0)
+{
+    Console.WriteLine("Harry found an ostracon, which is dated to the 6th century BCE.");
+    if (tools.Count != 0)
+    {
+        Console.WriteLine($"Tools: {string.Join(", ", tools)}");
+    }
+    if (substances.Count != 0)
+    {
+        Console.WriteLine($"Substances: {string.Join(", ", substances)}");
+    }
+}
---
+++ 09. Exam Preparation/02. Mouse In The Kitchen/Program.cs
@@ -0,0 +1,130 @@
+﻿int[] matrixSize = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+int rows = matrixSize[0];
+int cols = matrixSize[1];
+
+char[,] matrix = new char[rows, cols];
+
+string command;
+int mouseRow = 0;
+int mouseCol = 0;
+int cheeseCounter = 0;
+
+
+for (int row = 0; row < rows; row++)
+{
+    string colInput = Console.ReadLine();
+    for (int col = 0; col < cols; col++)
+    {
+        matrix[row, col] = colInput[col];
+
+        if (matrix[row,col] == 'M')
+        {
+            mouseRow = row;
+            mouseCol = col;
+            matrix[row, col] = '*';
+        }
+        if (matrix[row, col] == 'C')
+        {
+            cheeseCounter += 1;
+        }
+    }
+}
+
+while ((command = Console.ReadLine()) != "danger")
+{
+    if (command == "left")
+    {
+        if (mouseCol == 0)
+        {
+            Console.WriteLine("No more cheese for tonight!");
+            matrix[mouseRow, mouseCol] = 'M';
+            break;
+        }
+        if (matrix[mouseRow, mouseCol - 1] == '@')
+        {
+            continue;
+        }
+        mouseCol -= 1;
+
+        
+    }
+    if (command == "right")
+    {
+        if (mouseCol == cols - 1)
+        {
+            Console.WriteLine("No more cheese for tonight!");
+            matrix[mouseRow, mouseCol] = 'M';
+            break;
+        }
+        if (matrix[mouseRow, mouseCol + 1] == '@')
+        {
+            continue;
+        }
+        mouseCol += 1;
+        
+    }
+    if (command == "up")
+    {
+        if (mouseRow == 0)
+        {
+            Console.WriteLine("No more cheese for tonight!");
+            matrix[mouseRow, mouseCol] = 'M';
+            break;
+        }
+
+        if (matrix[mouseRow - 1, mouseCol] == '@')
+        {
+            continue;
+        }
+        mouseRow -= 1;
+
+    }
+    if (command == "down")
+    {
+        if (mouseRow == rows - 1)
+        {
+            Console.WriteLine("No more cheese for tonight!");
+            matrix[mouseRow, mouseCol] = 'M';
+            break;
+        }
+        if (matrix[mouseRow + 1, mouseCol] == '@')
+        {
+            continue;
+        }
+        mouseRow += 1;
+    }
+
+    if (matrix[mouseRow, mouseCol] == 'C')
+    {
+        matrix[mouseRow, mouseCol] = '*';
+        cheeseCounter--;
+        if (cheeseCounter == 0)
+        {
+            Console.WriteLine("Happy mouse! All the cheese is eaten, good night!");
+            matrix[mouseRow, mouseCol] = 'M';
+            break;
+        }
+    }
+    else if (matrix[mouseRow, mouseCol] == 'T')
+    {
+        matrix[mouseRow, mouseCol] = 'M';
+        Console.WriteLine("Mouse is trapped!");
+        break;
+    }
+}
+
+if (command == "danger")
+{
+    Console.WriteLine("Mouse will come back later!");
+}
+matrix[mouseRow, mouseCol] = 'M';
+
+for (int row = 0; row < rows; row++)
+{
+    for (int col = 0; col < cols; col++)    
+    {
+        Console.Write(matrix[row,col]);
+    }
+    Console.WriteLine();
+}

5ae16a8fd6d753c6297f604cfe4fb3550b185d68
Amxx79 <e.siwanov@gmail.com>
2023-10-18 18:54:21 +0300

    Exam Preparation

---
+++ 10. Exam Preparation/03. Vending System/Drink.cs
@@ -0,0 +1,23 @@
+﻿namespace VendingSystem
+{
+    public class Drink
+    {
+        public Drink(string name, decimal price, int volume)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Volume = volume;
+        }
+
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Volume { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Price: ${Price}, Volume: {Volume} ml";
+        }
+    }
+
+
+}

48a263551109550f8c5bb710214078c298cf91f6
Amxx79 <e.siwanov@gmail.com>
2023-10-21 11:53:50 +0300

    Exam

---
+++ 12. Exam/03. MailClient/Mail.cs
@@ -0,0 +1,23 @@
+﻿namespace MailClient
+{
+    public class Mail
+    {
+        public Mail(string sender, string receiver, string body)
+        {
+            Sender = sender;
+            Receiver = receiver;
+            Body = body;
+        }
+
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public string Body { get; set; }
+
+        public override string ToString()
+        {
+            return $"From: {this.Sender} / To: {this.Receiver}" + Environment.NewLine +
+                $"Message: {this.Body}";
+
+        }
+    }
+}
---
+++ 12. Exam/03. MailClient/MailBox.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics.Metrics;
+using System.Text;
+
+namespace MailClient
+{
+    public class MailBox
+    {
+        public MailBox(int capacity)
+        {
+            Capacity = capacity;
+            Inbox = new List<Mail>();
+            Archive = new List<Mail>();
+        }
+
+        public int Capacity { get; set; }
+        public List<Mail> Inbox { get; set; }
+        public List<Mail> Archive { get; set; }
+
+        public void IncomingMail(Mail mail)
+        {
+            if (Capacity > Inbox.Count)
+            {
+                Inbox.Add(mail);
+            }
+        }
+        public bool DeleteMail(string sender)
+        {
+            return Inbox.Remove(Inbox.FirstOrDefault(m => m.Sender == sender));
+        }
+        public int ArchiveInboxMessages()
+        {
+            int counter = 0;
+            for (int i = 0; i < Inbox.Count; i++)
+            {
+                Archive.Add(Inbox[i]);
+                counter++;
+            }
+            Inbox.Clear();
+            return counter;
+        }
+        public string GetLongestMessage()
+        {
+            return Inbox.MaxBy(m => m.Body.Length).ToString();
+        }
+        public string InboxView()
+        {
+            StringBuilder sb = new();
+            sb.AppendLine("Inbox:");
+            foreach (Mail mail in Inbox)
+            {
+                sb.AppendLine(mail.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+
+
+}

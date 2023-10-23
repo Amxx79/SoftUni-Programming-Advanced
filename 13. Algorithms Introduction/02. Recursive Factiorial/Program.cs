940b006c36f234fca88786ee5f656ef5d06aa4b9
Amxx79 <e.siwanov@gmail.com>
2023-10-23 19:39:15 +0300

    Algorithms Introduction

---
+++ 13. Algorithms Introduction/02. Recursive Factiorial/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = int.Parse(Console.ReadLine());
+Console.WriteLine(NFactorial(n));
+
+long NFactorial(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    return n * NFactorial(n - 1);
+}
